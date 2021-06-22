46abfa63196fa11dad9d638f1e029a9d2998fe99
moseqmoseg <77445489+moseqmoseg@users.noreply.github.com>
2021-06-22 13:47:23 +0200

    Completed

--- modules/hover.js
+++ modules/hover.js
@@ -1,17 +1,16 @@
-function hoverHeader () {
-    const header = document.querySelector(".header");
-    const modal = document.getElementById("myModal");
-    header.addEventListener("mouseover", e => {
-        modal.style.display = "block"
-    });
+ export function hoverHeader() {
+     const header = document.querySelector(".header");
+     const modal = document.getElementById("myModal");
+     header.addEventListener("mouseover", e => {
+         modal.style.display = "block"
+     });
 
-    modal.addEventListener("click", (e) => {
-        switch(e.target.className) {
-            case "close":
-            case "modal":
-            modal.style.display = "none"
-            break;
-        }
-    })
-}
-export {hoverHeader};
+     modal.addEventListener("click", (e) => {
+         switch (e.target.className) {
+             case "close":
+             case "modal":
+                 modal.style.display = "none"
+                 break;
+         }
+     })
+ };
